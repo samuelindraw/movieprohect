931e6879601d184df85ff55bae760e055d6a5329
samuel indra wijaya <samuel.indra@si.ukdw.ac.id>
2018-09-17 03:32:36 -0700

    test
    test

---
+++ MovieProject_deploy/js/javascript.js
@@ -0,0 +1,35 @@
+﻿window.onload = function () {
+        var chart = new CanvasJS.Chart("Report", {
+            animationEnabled: true,
+
+            title: {
+                text: "REPORT"
+            },
+            axisX: {
+                interval: 1
+            },
+            axisY2: {
+                interlacedColor: "#EEE0B1",
+                gridColor: "rgba(0,255,255,.3)"
+            },
+            data: [{
+                type: "bar",
+                name: "movie",
+                axisYType: "secondary",
+                color: "#A1CAF1",
+                dataPoints: [
+                    { y: 2, label: "" },
+                    { y: 2, label: "Animation" },
+                    { y: 2, label: "Comedy" },
+                    { y: 1, label: "Crime" },
+                    { y: 6, label: "Drama" },
+                    { y: 2, label: "Mysteri" },
+                    { y: 7, label: "Sci-Fe" },
+                    { y: 6, label: "Thriller" }
+                ]
+            }]
+        });
+        chart.render();
+    }
+
+
